e73d3f5feb4a2e9572877af0e43761fa52a0e6a2
Pbinh <pbinhf@gmail.com>
2025-11-02 18:25:00 +0700

    feat: implement Day model to manage daily tasks and serialization

--- src/main/java/taskmanagement/models/Day.java
+++ src/main/java/taskmanagement/models/Day.java
@@ -1,4 +1,68 @@
 package taskmanagement.models;
 
-public class Day {
+// Day object chứa các task và xử lý thêm hay xóa task
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.io.Serial;
+import java.io.Serializable;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class Day implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final LocalDate date;
+    /* Dùng observable list để list view phần UI tự động cập nhật danh sách các task
+    Observable list không lưu được nên để transient và tạo 1 list khác chỉ để lưu */
+    private transient ObservableList<Task> taskObservableList;
+    private List<Task> serializableList;
+
+    public Day(LocalDate date) {
+        this.date = date;
+        this.taskObservableList = FXCollections.observableArrayList();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void addTask(Task task) {
+        taskObservableList.add(task);
+        sortTasksByTime();
+    }
+
+    private void sortTasksByTime() {
+        taskObservableList.sort(Comparator.comparing(Task::getStartTime));
+    }
+
+    public void removeTask(Task task) {
+        taskObservableList.remove(task);
+    }
+
+    public ObservableList<Task> getTaskObservableList() {
+        return taskObservableList;
+    }
+
+    // Copy các task trong observable list để lưu lại
+    @Serial
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        serializableList = new ArrayList<>(taskObservableList);
+        oos.defaultWriteObject();
+    }
+
+    // Tạo lại observable list
+    @Serial
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        ois.defaultReadObject();
+        taskObservableList = FXCollections.observableArrayList(serializableList);
+    }
+
 }
