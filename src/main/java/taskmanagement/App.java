955b2c33c21a5faa64ca7265e895bfa0dd0ae362
Pbinh <pbinhf@gmail.com>
2025-11-02 18:52:33 +0700

    feat: implement App and AppManager for JavaFX initialization and scene navigation

--- src/main/java/taskmanagement/App.java
+++ src/main/java/taskmanagement/App.java
@@ -1,4 +1,34 @@
 package taskmanagement;
 
-public class App {
+import taskmanagement.models.Calendar;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void init() {
+        AppManager.calendar = new Calendar();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        AppManager.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/Fxml/main-window.fxml"));
+        AppManager.mainWindow = new Scene(fxmlLoader.load());
+        AppManager.switchToMainWindow();
+    }
+
+    @Override
+    public void stop() throws IOException {
+        AppManager.calendar.saveWeeksToFile();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
