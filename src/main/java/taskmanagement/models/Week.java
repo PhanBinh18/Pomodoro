703e5c6ad79d02676e276f40e88679b02273571e
Pbinh <pbinhf@gmail.com>
2025-11-02 18:26:01 +0700

    feat: implement Week model to manage list of days in a week

--- src/main/java/taskmanagement/models/Week.java
+++ src/main/java/taskmanagement/models/Week.java
@@ -1,4 +1,33 @@
 package taskmanagement.models;
 
-public class Week {
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Week implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final List<Day> dayList;
+    private final LocalDate startDate;
+
+    public Week(LocalDate startDate) {
+        this.startDate = startDate;
+        this.dayList = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            LocalDate dayDate = startDate.plusDays(i);
+            dayList.add(new Day(dayDate));
+        }
+    }
+
+    public List<Day> getDayList() {
+        return dayList;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
 }
