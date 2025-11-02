b5dbf528e5e7ee49680ecf436d10daed12cb6fc2
Pbinh <pbinhf@gmail.com>
2025-11-02 18:15:33 +0700

    Initial project setup with Maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module com.btl.taskmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
+    requires java.desktop;
+
+    opens taskmanagement to javafx.fxml;
+    opens taskmanagement.Controllers to javafx.fxml;
+    exports taskmanagement;
+    exports taskmanagement.models;
+    exports taskmanagement.controllers;
+}
